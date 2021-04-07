66497a403c393ff8720745d21662a4d6d802f764
Mollymango <stoffe-1990@hotmail.com>
2021-04-07 22:32:49 +0200

    labb finished

---
+++ Core/Inc/main.h
@@ -0,0 +1,101 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.h
+  * @brief          : Header for main.c file.
+  *                   This file contains the common defines of the application.
+  ******************************************************************************
+  * @attention
+  *
+  * <h2><center>&copy; Copyright (c) 2021 STMicroelectronics.
+  * All rights reserved.</center></h2>
+  *
+  * This software component is licensed by ST under BSD 3-Clause license,
+  * the "License"; You may not use this file except in compliance with the
+  * License. You may obtain a copy of the License at:
+  *                        opensource.org/licenses/BSD-3-Clause
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __MAIN_H
+#define __MAIN_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Includes ------------------------------------------------------------------*/
+#include "stm32wbxx_hal.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+
+/* USER CODE END Includes */
+
+/* Exported types ------------------------------------------------------------*/
+/* USER CODE BEGIN ET */
+
+/* USER CODE END ET */
+
+/* Exported constants --------------------------------------------------------*/
+/* USER CODE BEGIN EC */
+
+/* USER CODE END EC */
+
+/* Exported macro ------------------------------------------------------------*/
+/* USER CODE BEGIN EM */
+
+/* USER CODE END EM */
+
+/* Exported functions prototypes ---------------------------------------------*/
+void Error_Handler(void);
+
+/* USER CODE BEGIN EFP */
+
+/* USER CODE END EFP */
+
+/* Private defines -----------------------------------------------------------*/
+#define STOFFELD0_Pin GPIO_PIN_0
+#define STOFFELD0_GPIO_Port GPIOA
+#define STOFFELD1_Pin GPIO_PIN_1
+#define STOFFELD1_GPIO_Port GPIOA
+#define STOFFELD2_Pin GPIO_PIN_2
+#define STOFFELD2_GPIO_Port GPIOA
+#define STOFFELD3_Pin GPIO_PIN_3
+#define STOFFELD3_GPIO_Port GPIOA
+#define STOFFELD4_Pin GPIO_PIN_4
+#define STOFFELD4_GPIO_Port GPIOA
+#define STOFFELD5_Pin GPIO_PIN_5
+#define STOFFELD5_GPIO_Port GPIOA
+#define STOFFELD6_Pin GPIO_PIN_6
+#define STOFFELD6_GPIO_Port GPIOA
+#define KNAPPEN_Pin GPIO_PIN_4
+#define KNAPPEN_GPIO_Port GPIOC
+#define JTMS_Pin GPIO_PIN_13
+#define JTMS_GPIO_Port GPIOA
+#define JTCK_Pin GPIO_PIN_14
+#define JTCK_GPIO_Port GPIOA
+#define B2_Pin GPIO_PIN_0
+#define B2_GPIO_Port GPIOD
+#define B3_Pin GPIO_PIN_1
+#define B3_GPIO_Port GPIOD
+#define JTDO_Pin GPIO_PIN_3
+#define JTDO_GPIO_Port GPIOB
+#define STLINK_RX_Pin GPIO_PIN_6
+#define STLINK_RX_GPIO_Port GPIOB
+#define STLINK_TX_Pin GPIO_PIN_7
+#define STLINK_TX_GPIO_Port GPIOB
+/* USER CODE BEGIN Private defines */
+
+/* USER CODE END Private defines */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __MAIN_H */
+
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
